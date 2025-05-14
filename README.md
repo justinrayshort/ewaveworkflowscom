75d578347a2072f366ea31d2fd5634f359e0538f
Justin Short <justin@jshort.org>
2025-05-14 15:59:15 -0700

    V0.1 Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# EcoWave Workflows Website
+
+## Website Infrastructure
+
+- SvelteKit
+- Cloudflare
+
+## Summary
+
+This is the initial draft for [ecowaveworkflows.com](https://ecowaveworkflows.com/).
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<div class="flex items-center justify-center">
+	<div class="w-full  sm:w-1/2 md:max-w-1/2">
+		<img alt="AI technology" src="/ai2.jpg" />
+		<div class="hero-content text-neutral-content text-center">
+			<div class="">
+				<h1 class="mb-2 text-5xl font-bold">EcoWave Workflows</h1>
+				<p class="p-4">
+					We specialize in designing workflows that are custom tailored to your business needs.
+				</p>
+				<p class="p-4">
+					Our team of experts will work with you to create a customized solution that streamlines
+					your processes and improves efficiency. With EcoWave, you can expect a seamless
+					integration of technology and human interaction, resulting in a more productive and
+					satisfying experience for both you and your customers.
+				</p>
+				<p class="p-4 mb-4">Click below to contact one of our team members!</p>
+				<button class="btn btn-primary"
+					><a href="mailto:sales@ecowaveworkflows.com">Get Started!</a></button
+				>
+			</div>
+		</div><img alt="AI technology" src="/workflows.png" />
+	</div>
+
+	<!-- <h1 class="text-4xl pt-10">Take your business to the next level</h1>
+<p class="pt-10">EcoWave for your business makes it easier than ever to engage with all of your customers in a natural and expedient way.
+    We specialize in designing and implementing workflows that are tailored to your specific needs. Our team of experts will work with you to create a customized solution that streamlines your processes and improves efficiency. With EcoWave, you can expect a seamless integration of technology and human interaction, resulting in a more productive and satisfying experience for both you and your customers.
+    Whether you need help with customer service, sales, or any other aspect of your business, we have the tools and expertise to help you succeed. Let us help you take your business to the next level with EcoWave.
+</p> -->
+</div>
